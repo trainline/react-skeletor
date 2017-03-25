560454947b60ae3e56156f2dc33cd0a8096442f4
Jamie Copeland <Jamie.Copeland@thetrainline.com>
2017-03-25 08:29:41 +0000

    Update dependencies and tidy tsconfig

--- src/pendify.tsx
+++ src/pendify.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export function pendify() {
-  const WrappedComponent: React.StatelessComponent<void> = () => <div>pendify</div>;
+export function pendify<TProps>(Component: React.StatelessComponent<TProps>) {
+  const WrappedComponent: React.StatelessComponent<void> = () => <div>Pendify...</div>;
   return WrappedComponent;
 }
