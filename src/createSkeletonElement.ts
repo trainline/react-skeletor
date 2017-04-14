39ddfa98d2f7cbdf50d7d32edec20f819c4e2863
Jamie Copeland <Jamie.Copeland@thetrainline.com>
2017-04-14 12:00:50 +0100

    Tidy className and style prop passing

--- src/createSkeletonElement.ts
+++ src/createSkeletonElement.ts
@@ -2,16 +2,25 @@ import * as React from 'react';
 
 import { contextTypes, Context, Styling } from './utils';
 
+const createCustomStyle = (isPending: boolean, prop?: React.CSSProperties, context?: React.CSSProperties, custom?: React.CSSProperties) => ({
+  ...prop,
+  ...(isPending ? context : undefined),
+  ...(isPending ? custom : undefined)
+})
+
+const createClassName = (isPending: Boolean, prop?: string, context?: string, custom?: string) => (
+  (prop ? `${prop} ` : '') +
+  (context ? `${context} ` : '') +
+  (custom ? `${custom} ` : '')
+);
+
 export const createSkeletonElement = (customStyling?: Styling) => (type: any) => {
   const ExportedComponent: React.StatelessComponent<any> = (
     props: any, { skeletor: { isPending, styling } }: Context
   ) => React.createElement(type, {
     ...props,
-    style: {
-      ...props.style,
-      ...(isPending && ((customStyling && customStyling.style) || (styling && styling.style)) || {}),
-    },
-    className: `${props.className}${isPending && (customStyling && (customStyling.className) || (styling && styling.className))}` // tslint:disable-line:max-line-length
+    style: createCustomStyle(isPending, props.style, styling && styling.style, customStyling && customStyling.style),
+    className: createClassName(isPending, props.className, styling && styling.className, customStyling && customStyling.className)
   });
 
   ExportedComponent.contextTypes = contextTypes;
