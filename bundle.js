adfae4f39308019acdc2e0e121fc0dc6eeb7ad9c
Alexandre Rieux <alexr.3165@gmail.com>
2017-06-26 08:54:51 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "700174f73e1666e0d91d"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "98ca86b223bded0c8f7f"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
@@ -55280,8 +55280,8 @@ var ExternalLink = (_d = ["\n  margin: 0px 8px;\n  color: ", ";\n  text-decorati
 var Header = (_e = ["\n  padding: 20px;\n"], _e.raw = ["\n  padding: 20px;\n"], styled_components_1.default.header(_e));
 var pathname = react_router_1.browserHistory.getCurrentLocation().pathname;
 var paths = [
-    NODE_ENV === 'prod' ? '/react-mapbox-gl' : '/',
-    NODE_ENV === 'prod' ? '/react-mapbox-gl/demo' : '/demo'
+    NODE_ENV === 'prod' ? '/react-skeletor' : '/',
+    NODE_ENV === 'prod' ? '/react-skeletor/demo' : '/demo'
 ];
 var selected = pathname === paths[0] ? 0 : 1;
 var Root = function (_a) {
